03c9786368d821520bf3810eb54340e66c70a397
Vartan Vardanyan <gremendousip@gmail.com>
2022-05-15 16:35:28 +0300

    Убрана кнопка "Калькулятор калорий".

--- keyboards/default/menu.py
+++ keyboards/default/menu.py
@@ -2,18 +2,12 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 NUTRITION = "🥑 Питание"
 WORKOUTS = "💪 Тренировки"
-CAL_CALCULATOR = "🥦 Калькулятор калорий"
 ABOUT_USER = "📜 Информация о себе"
 
 menu = ReplyKeyboardMarkup(
     keyboard=[
-        [
-            KeyboardButton(NUTRITION),
-            KeyboardButton(WORKOUTS),
-        ],
-        [
-            KeyboardButton(CAL_CALCULATOR),
-            KeyboardButton(ABOUT_USER)
-        ],
+        [KeyboardButton(NUTRITION)],
+        [KeyboardButton(WORKOUTS)],
+        [KeyboardButton(ABOUT_USER)]
     ], resize_keyboard=True
 )
